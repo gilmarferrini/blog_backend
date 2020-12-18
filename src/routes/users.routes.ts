b1c6ad4f48f247294b849d50324001c90a184969
gilmarferrini <ferrinigilmar@gmail.com>
2020-12-18 09:42:21 -0300

    Refactoring

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -8,7 +8,6 @@ const userController = new UserController();
 usersRouter.get('/', async (request, response) => {
   try {
     const users = await userController.index();
-    console.log('foi chamado');
 
     return response.status(200).json(users);
   } catch (e) {
