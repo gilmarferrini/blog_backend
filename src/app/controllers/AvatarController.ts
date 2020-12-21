703177df2421ef1c0d957c085bd13a7e5b2ff394
gilmarferrini <ferrinigilmar@gmail.com>
2020-12-21 09:02:10 -0300

    Added AvatarController

---
+++ src/app/controllers/AvatarController.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import fs from 'fs';
+import path from 'path';
+
+import User from '../models/User';
+
+const uploadFolder = path.resolve(__dirname, '..', '..', '..', 'uploads');
+
+interface IRequestUpdate {
+  id: string;
+  avatar: string;
+}
+
+class AvatarController {
+  public static async delete(filename: string): Promise<void> {
+    const avatarPath = path.join(__dirname, '..', '..', '..', 'tmp', filename);
+    await fs.promises.unlink(avatarPath);
+  }
+
+  public async update({ id, avatar }: IRequestUpdate): Promise<void> {
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new Error('Nenhum funcionário com esse id encontrado');
+    }
+
+    if (user.avatar) {
+      const userAvatarPath = path.join(uploadFolder, user.avatar);
+      const userAvatarStats = await fs.promises.stat(userAvatarPath);
+
+      if (userAvatarStats) {
+        await fs.promises.unlink(userAvatarPath);
+      }
+    }
+
+    user.avatar = avatar;
+
+    await userRepository.save(user);
+  }
+}
+
+export default AvatarController;
