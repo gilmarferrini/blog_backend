a7fe8621947667973dc9f158584123f36ba52822
gilmarferrini <ferrinigilmar@gmail.com>
2021-01-17 19:14:16 -0300

    added auth router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 
 import usersRouter from './users.routes';
 import postsRouter from './posts.routes';
+import authRouter from './auth.routes';
 
 const routes = Router();
 
+routes.use('/auth', authRouter);
 routes.use('/users', usersRouter);
 routes.use('/post', postsRouter);
 
