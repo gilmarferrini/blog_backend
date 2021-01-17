95a4a1012cd033f75b5652d644bd9098fa67e490
gilmarferrini <ferrinigilmar@gmail.com>
2021-01-17 19:13:55 -0300

    added middleware

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
-
+import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
